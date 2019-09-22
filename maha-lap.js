f4506e9b262aa9ef08a694a77105c4cc18644039
zanzanzanm03 <olafminister12365@hotmail.com>
2019-09-22 13:09:56 +0700

    maha lab 3

--- maha-lap.js
+++ maha-lap.js
@@ -1,77 +1,81 @@
 class MahaLap {
-    static tamnaiWithDay(day) {
-        switch (day) {
-            case "อาทิตย์":
-                return 1;
-            case "จันทร์":
-                return 2;
-            case "อังคาร":
-                return 3;
-            case "พุธ":
-                return 4;
-            case "พฤหัสบดี":
-                return 5;
-            case "ศุกร์":
-                return 6;
-            case "เสาร์":
-                return 7;
-            case "สงกรานต์":
-                return 13;
-            case "ฮาโลวีน":
-                return 31;
-            case "คริสมาสต์":
-                return 25;
-            default:
-                return "Not Match";
-        }
-
+  static tamnaiWithDay(day) {
+    switch (day) {
+      case "อาทิตย์":
+        return 1;
+      case "จันทร์":
+        return 2;
+      case "อังคาร":
+        return 3;
+      case "พุธ":
+        return 4;
+      case "พฤหัสบดี":
+        return 5;
+      case "ศุกร์":
+        return 6;
+      case "เสาร์":
+        return 7;
+      case "สงกรานต์":
+        return 13;
+      case "ฮาโลวีน":
+        return 31;
+      case "คริสมาสต์":
+        return 25;
+      default:
+        return "Not Match";
     }
+  }
 
-    static tamnaiWithStory(story) {
-        const length = story.length;
-        var sum = 0;
-        for (var i = 1; i <= length; i++) {
-            sum = sum + i;
-        }
-        switch (sum) {
-            case 3:
-                return sum;
-            case 36:
-                return sum;
-            case 66:
-                return sum;
-            default:
-                "Not Match"
-        }
-
+  static tamnaiWithStory(story) {
+    const length = story.length;
+    var sum = 0;
+    for (var i = 1; i <= length; i++) {
+      sum = sum + i;
     }
-
-    // static tamnaiWithStory(story) {
-    //     const n = story.length;
-    //     let sum = 0;
-    //     for (let i = 1; i <= n; i++) {
-    //       sum += i;
-    //     }
-    //     return sum % 100;
-    //   }
-
-    static tamnaiWithTime(time) {
-        const randomNumber = Math.random();
-        if (time === "กลางวัน") {
-            if (randomNumber >= 0.5) {
-                return randomNumber;
-            } else {
-                return 0.5
-            }
-        }
-        if (time === "กลางคืน") {
-            if (randomNumber >= 0.5) {
-                return 0.5;
-            } else {
-                return randomNumber;
-            }
-        }
+    switch (sum) {
+      case 3:
+        return sum;
+      case 36:
+        return sum;
+      case 66:
+        return sum;
+      default:
+        "Not Match";
     }
+  }
+
+  // static tamnaiWithStory(story) {
+  //     const n = story.length;
+  //     let sum = 0;
+  //     for (let i = 1; i <= n; i++) {
+  //       sum += i;
+  //     }
+  //     return sum % 100;
+  //   }
+
+  static tamnaiWithTime(time) {
+    const randomNumber = Math.random();
+    if (time === "กลางวัน") {
+      if (randomNumber >= 0.5) {
+        return randomNumber;
+      } else {
+        return 0.5;
+      }
+    }
+    if (time === "กลางคืน") {
+      if (randomNumber >= 0.5) {
+        return 0.5;
+      } else {
+        return randomNumber;
+      }
+    }
+  }
+  static namo(day, story, time) {
+    const resultday = this.tamnaiWithDay(day);
+    const resulstory = this.tamnaiWitstory(story);
+    const resulttime = this.tamnaiWithtime(time);
+    return (resultday * resulstory * resulttime * 100) % 1000;
+  }
 }
 
 module.exports = MahaLap;
